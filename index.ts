473a5f5fbc9dec96d5d7338cf7d61bcdb9a8738e
Zenepity <jacksonw@gmx.us>
2022-08-05 21:43:29 -0400

    deprecate type argument, add express example

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
-export async function logger(text: string, level?: number, type?: string) {
+export async function logger(
+	text: string = 'Please provide a valid log message',
+	level: number = 1337
+) {
 	function isNumber(n: any) {
 		return /^-?[\d.]+(?:e-?\d+)?$/.test(n);
 	}
 
-	if (!level && !type)
-		throw new TypeError('Must provide either level or type property.');
-	if (level && type)
-		throw new TypeError('Must only provide either level or type property.');
+	if (!level)
+		throw new TypeError('Must provide a level, examples: 1, 2, 3, 4, 5, 1337');
 	if (isNumber(level) === false) throw new TypeError('Level must be a number.');
-	if (isNumber(type) === true) throw new TypeError('Type must be a string.');
 	if (isNumber(text) === true) throw new TypeError('Text must be a string.');
 
 	return console.log('h');
